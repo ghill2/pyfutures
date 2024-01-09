bf23c8c8b530f6a9368bad286f69e4537a073580
George Hill <georgehill010@gmail.com>
2024-01-09 14:10:42 +0000

    auto commit 2024-01-09 14:10:42

--- pyfutures/tests/adapters/interactive_brokers/demo/import_instrument_json.py
+++ pyfutures/tests/adapters/interactive_brokers/demo/import_instrument_json.py
@@ -6,9 +6,6 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 from bs4 import Tag
 
-from pyfutures.data.ib.exceptions import ScraperDownloadFailedError
-
-
 def get_all_text_recursive(element):
     """
     Returns all text recursively within the given element.
@@ -37,40 +34,34 @@ class ImportInstrumentJson:
         html_files = list(path.rglob("*.html"))
 
         for path in html_files:
-            data = self.parse_html_file(path=path)
+            print(path)
+            exit()
+            with open(path) as f:
+                data = self.parse_data(html_data=f.read())
 
             if write:
                 outpath = self._out_folder / f"{path.stem}.json"
                 with open(outpath, "w") as f:
                     json.dump(data, f, indent=4)
 
-    def parse_html_file(self, path: Path):
-        path = Path(path)
-        print(f"Parsing {path}")
-
-        with open(path) as f:
-            try:
-                data = self.parse_data(html_data=f.read())
-            except ValueError as e:
-                raise e
-            else:
-                return data
-
     def parse_data(self, html_data):
+        
         soup = BeautifulSoup(html_data, "html.parser")
         tables = soup.select("#contractSpecs > table")  # only get first child tables
 
         data = {}
         if len(tables) > 1:
             for table in tables:
+                print(table)
+                
                 self._scrape_table(data, table)
         else:
             print(f"Invalid Scraper HTML {html_data}")
             exit()
-
+        exit()
         # redownload
         if data["Underlying Information"]["Description/Name"] == "(@)":
-            raise ScraperDownloadFailedError(f"Failed Instrument: {data}")
+            raise ValueError(f"Failed Instrument: {data}")
 
         data = self.validate(data)
 
@@ -134,11 +125,6 @@ class ImportInstrumentJson:
                 # raise ValueError(f"property already exists, {k}")
                 d.setdefault(k, vv)
 
-        # if "Liquid Trading Hours" not in d:
-        #     print("==========")
-        #     print(d["Exchange"], d["Symbol"])
-        #     pprint.pprint(data)
-
         if "Trading Hours" in d:
             d["Trading Hours"] = self._validate_trading_hours(d["Trading Hours"])
         if "Liquid Trading Hours" in d:
@@ -158,17 +144,12 @@ class ImportInstrumentJson:
             print("================")
             print("MULTIPLIER: ", e)
             pprint.pprint(d)
-            # exit()
-
         try:
             d["Closing Price"] = float(d["Closing Price"])
         except Exception as e:
             print("================")
             print("CLOSING PRICE: ", e)
             pprint.pprint(d)
-            # exit()
-        # pprint.pprint(d)
-
         return d
 
     def _scrape_inner_table(self, table):
@@ -188,9 +169,6 @@ class ImportInstrumentJson:
         """
         Recursively scrape table.
         """
-        # print("===========================================")
-        # print(table)
-        # print("===========================================")
         try:
             table_title = table.select(".underlying")[0].select("th")[0].text.strip()
         except:
@@ -199,7 +177,6 @@ class ImportInstrumentJson:
 
         for tr in table.children:
             if isinstance(tr, Tag):
-                # print(tr)
                 # sometimes the row is 2 different structures:
                 # th->title, td->value
                 # td->title, td->value
@@ -225,178 +202,3 @@ class ImportInstrumentJson:
 
 if __name__ == "__main__":
     ImportInstrumentJson().import_all()
-    # def universe_instruments(self, write: bool = True):
-    #     from pyfutures.instruments.universe.provider import UniverseProvider
-
-    #     for instrument_id in UniverseProvider.instrument_ids_with_exchange():
-    #         exchange = instrument_id.venue
-    #         symbol = instrument_id.symbol
-    #         path = PACKAGE_ROOT / f"data/ib/instruments/info_html/{exchange}-{symbol}.html"
-    #         if not path.exists():
-    #             print(f"File does not exist - {path}")
-    #             continue
-    #         try:
-    #             data = self.parse_html_file(path=path)
-    #         except ScraperDownloadFailedError as e:
-    #             print(e)
-    #             pass
-    #         outpath = PACKAGE_ROOT / f"data/ib/instruments/info_json/{exchange}-{symbol}.json"
-    #         if write:
-    #             with open(outpath, "w") as f:
-    #                 json.dump(data, f, indent=4)
-
-    # if i < 1:
-    #     continue
-
-    # if tr.name == "th":
-    # if len(tr.select("th")) == 1 and len(tr.select("td") == 0):
-    # its a title
-
-    # tr_children = list(tr.children)
-    # row_title = next(tr.children).text
-    # 1st child of tr is the row title
-    # row_title = tr_children[0].text
-    # 2nd is the value and can either be text only or another table
-    #
-    # match tname:
-    #     case "Underlying Information" | "Contract Information" | "Stock Features" | "Margin Requirements":
-    #         tdata = self.scrape_table(table)
-    #         data[tname] = tdata
-    #     case "Contract Identifiers":
-    #         tdata = self.scrape_table(table)
-    #         rep_tdata = {}
-    #         for key in ["ASSETID", "CONID", "ISIN"]:
-    #             for tkey, tvalue in tdata.items():
-    #                 if tkey.startswith(key):
-    #                     rep_tdata[key] = tvalue
-    #         print(tdata)
-    #         data[tname] = rep_tdata
-    #
-    #     case _:
-    #         # Trading Hours / Price & Size parameters
-    #         tdata = self.scrape_table(table)
-    #
-    #         if "Price Paramaters" in tdata:
-    #             price_params = re.sub(r"\n+", "\n", tdata["Price Parameters"]).split("\n")
-    #             print("PRICE PARAMS")
-    #             print(price_params)
-    #
-    #         data[tname] = tdata
-    #
-
-
-# def scrape_table(self, table):
-#     """bs4 table -> list[dict]"""
-#     data = {}
-#     data = self._scrape_table(table)
-#     # return {tr.select("th")[0].text: tr.select("td")[0].text for tr in table.select("tr")}
-#
-#     # print(f"================= {tname} ======================")
-#     # if re.match(r"^.*\(.*\) Top$", ):
-#     # exchange table has Price Parameters, Size Parameters, Trading Hours as inner tables
-#     # inner_tables = table.select("table")
-#     # print(inner_tables)
-#     # for inner_table in inner_tables:
-#     # print(data)
-#     # is exchange info table
-#
-#     # if the element contains another table, parse this instead
-#
-#     # else:
-#     # pass
-#     # data = self._scrape_table(table)
-#     # identify if the table name ends with (.*)$
-#     # this means its exchange info and contains trading hours, price params
-#     return data
-
-#
-# first_child = next(tr.children)
-# second_child = next(tr.children)
-# print("first: ", first_child, "second: ", second_child)
-
-# data = {}
-# column_names = [head.text for head in table.select("th")]
-# del column_names[0]
-# values = [row.text for row in table.select("td")]
-# for i, col_name in enumerate(column_names):
-#     try:
-#         data[col_name] = int(remove_all_occurrences(values[i], ","))
-#     except:
-#         # some tickers have margin as "Default"
-#         data[col_name] = values[i]
-
-# # Iterate over the rows
-# for row in rows:
-#     # Create an empty dictionary
-#     row_data = {}
-#
-#     # Iterate over the cells of the row
-#     cells = row.find_all("td")
-#
-#     # Add the text content of each cell to the dictionary
-#     for i, cell in enumerate(cells):
-#         row_data[column_names[i]] = cell.text
-#
-#     # Add the row dictionary to the list of rows
-#     rows_as_dicts.append(row_data)
-#
-# return [i for i in rows_as_dicts if i]  # filter empty dicts from list[dict]
-# return data
-
-
-# for i, table in enumerate(tables):
-#     print("========================================")
-#     print(f"===== TABLE {i} ====")
-#     print(table)
-#
-
-# ALWAYS use nordVPN proxies
-# so I can manually check the instrument pages without a proxy without having to enter captcha
-
-
-# current_proxy = {} if USE_PROXY else None
-
-
-# print(soup)
-# print(url)
-# print("====== we hit a captcha =====")
-# print("url copied to clipboard...")
-# input("Press Enter to Continue processing after manually entered captcha...")
-
-
-# def parse_margin_table(self, table):
-#     """bs4 table -> list[dict]"""
-#     # Iterate over the rows of the table
-#     rows = table.find_all("tr")
-#
-#     data = {}
-#     column_names = [head.text for head in table.select("th")]
-#     del column_names[0]
-#     values = [row.text for row in table.select("td")]
-#     print(values)
-#     for i, col_name in enumerate(column_names):
-#         try:
-#             data[col_name] = int(remove_all_occurrences(values[i], ","))
-#         except:
-#             # some tickers have margin as "Default"
-#             data[col_name] = values[i]
-#
-#     print(data)
-#
-#     # # Iterate over the rows
-#     # for row in rows:
-#     #     # Create an empty dictionary
-#     #     row_data = {}
-#     #
-#     #     # Iterate over the cells of the row
-#     #     cells = row.find_all("td")
-#     #
-#     #     # Add the text content of each cell to the dictionary
-#     #     for i, cell in enumerate(cells):
-#     #         row_data[column_names[i]] = cell.text
-#     #
-#     #     # Add the row dictionary to the list of rows
-#     #     rows_as_dicts.append(row_data)
-#     #
-#     # return [i for i in rows_as_dicts if i]  # filter empty dicts from list[dict]
-#     return data
--- pyfutures/tests/adapters/interactive_brokers/demo/scrape_instrument_html.py
+++ pyfutures/tests/adapters/interactive_brokers/demo/scrape_instrument_html.py
@@ -2,7 +2,10 @@ import json
 import time
 from pathlib import Path
 from dotenv import dotenv_values
-
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 import requests
 
 from pyfutures.tests.adapters.interactive_brokers.test_kit import IBTestProviderStubs
@@ -10,42 +13,79 @@ from pyfutures.tests.adapters.interactive_brokers.test_kit import IBTestProvider
 PROXY_EMAIL=dotenv_values()["PROXY_EMAIL"]
 PROXY_PASSWORD=dotenv_values()["PROXY_PASSWORD"]
 
+"""
+Scrapes the html of instruments in the universe
+quote_currency
+multiplier
+trading hours
+liquid trading hours
+"""
+
 class ScrapeInstrumentHtml:
     def __init__(self):
         self._out_folder = Path(__file__).parent / Path(__file__).stem
 
     def scrape_all(self):
-        path = Path(__file__).parent / "test_import_instruments.json"
-        with open(path) as f:
-            instruments: list[dict] = json.load(f)
-
+        
         universe = IBTestProviderStubs.universe_dataframe()
+        
+        
+        import geckodriver_autoinstaller
+        from selenium import webdriver
 
-        for symbol in universe.symbol:
-            outpath = self._out_folder / f"{symbol}.html"
+        geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
+                                            # and if it doesn't exist, download it automatically,
+                                            # then add geckodriver to path
+        from geckodriver_autoinstaller.utils import get_geckodriver_path
+        # from geckodriver_autoinstaller import get_geckodriver_filename
+        # Initialize Firefox WebDriver with proxy settings
+        # "http": 
+        # "https": f"{PROXY_EMAIL}:{PROXY_PASSWORD}@la.residential.rayobyte.com:8000",
+        proxy = Proxy({
+            'proxyType': ProxyType.MANUAL,
+            'httpProxy': f"{PROXY_EMAIL}:{PROXY_PASSWORD}@la.residential.rayobyte.com:8000",
+            'ftpProxy': 'proxy_server_address:port',
+            'sslProxy': 'proxy_server_address:port',
+            'noProxy': ''  # Optional: specify domains that should bypass the proxy, e.g., 'localhost,127.0.0.1'
+        })
+        
+        
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.headless = False  # Set to True if you want to run Firefox in headless mode
+        
+        # firefox_profile = webdriver.FirefoxProfile()
+        firefox_options.set_preference('network.proxy.type', 1)  # Manual proxy configuration
+        firefox_options.set_preference('network.proxy.http', 'la.residential.rayobyte.com')
+        firefox_options.set_preference('network.proxy.http_port', 8000)
+        firefox_options.set_preference('network.proxy.socks_username', PROXY_EMAIL)
+        firefox_options.set_preference('network.proxy.socks_password', PROXY_PASSWORD)
+        
+        from selenium.webdriver.firefox.service import Service
+        
+        driver = webdriver.Firefox(
+                    service=Service(
+                        executable_path=".venv/lib/python3.10/site-packages/geckodriver_autoinstaller/v0.34.0/geckodriver",
+                    ),
+                    options=firefox_options,
+                    )
+        url = "https://contract.ibkr.info/v3.10/index.php?action=Details&site=GEN&conid=54939868"
+        
+        for row in universe.itertuples():
+            
+            outpath = self._out_folder / f"{row.trading_class}.html"
 
-            if outpath.exists():
-                print(f"File Exists {outpath} - Skipping...")
-                continue
-
-            # find the symbol in the instrument list
-            matched = None
-            for instrument in instruments:
-                if instrument["ib_symbol"] == symbol:
-                    matched = instrument
-                    break
-            assert matched is not None, f"Could not find symbol {symbol}"
-            instrument = matched
-
-            url = instrument["url"]
+            # if outpath.exists():
+            #     print(f"File Exists {outpath} - Skipping...")
+            #     continue
 
             print(f"Processing {url}")
-            html = self.scrape(url)
+            driver.get(row.url)
+            html_content = driver.page_source
 
             print(f"--> Writing {outpath}")
             outpath.parent.mkdir(parents=True, exist_ok=True)
-            with open(outpath, "wb") as f:
-                f.write(html)
+            with open(outpath, "w") as f:
+                f.write(html_content)
 
             time.sleep(1)
 
@@ -67,13 +107,42 @@ class ScrapeInstrumentHtml:
         auth = HTTPProxyAuth(PROXY_EMAIL, PROXY_PASSWORD)
         
         r = requests.get(url, proxies=proxies, auth=auth)
-
+        print(r.content)
         assert "To continue please enter the text from the image below" not in r.text
         return r.content
 
     
 if __name__ == "__main__":
     url = "https://contract.ibkr.info/index.php?action=Details&site=GEN&conid=657796604"
-
     ScrapeInstrumentHtml().scrape_all()
     
+    # # assert "Python" in driver.title
+        # # exit()
+        # # from selenium import webdriver
+
+        # # # Path to the Chrome WebDriver executable
+        # # # driver_path = '/Users/g1/BU/projects/pytower_develop/.venv/lib/python3.10/site-packages/chromedriver_py/chromedriver_mac-x64'
+        # # from chromedriver_py import binary_path
+
+        # # options = ChromeOptions()
+        # # options.binary_location = binary_path
+        # # options.headless = True
+        # # options.add_argument("start-maximized"); # open Browser in maximized mode
+        # # options.add_argument("--disable-extensions") # disabling extensions
+        # # options.add_argument('--disable-logging')
+        # # options.add_argument('--remote-debugging-pipe')  # fix port error
+        # # options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
+        # # options.add_argument("--no-sandbox"); # Bypass OS security model
+        # # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # # options.add_argument("--disable-browser-side-navigation"); # https://stackoverflow.com/a/49123152/1689770
+        # # options.add_argument("--window-size=1920,1080")
+        
+        # # driver = webdriver.Chrome(
+        # #     service=Service(executable_path=binary_path),
+        # #     options=options
+        # # )
+
+        # # Open a website
+        # # driver.get('www.google.com')
+        
+        # exit()
