8c19dc6ee3778d7c73e2774083d285ee16cede04
George Hill <georgehill010@gmail.com>
2024-02-06 15:50:41 +0000

    auto commit 2024-02-06 15:50:41

--- pyfutures/tests/correlation.py
+++ pyfutures/tests/correlation.py
@@ -9,15 +9,38 @@ from pyfutures.continuous.adjusted import AdjustedPrices
 from pathlib import Path
 from nautilus_trader.model.enums import BarAggregation
 import polars as pl
+
+def merge_dataframes_pandas(dfs: list[pd.DataFrame]) -> pd.DataFrame:
+    total_count = len(dfs)
+    i = 0
+    total = pd.DataFrame(columns=["timestamp"])
+    while len(dfs) > 0:
+        df = dfs.pop(-1)
+        total = pd.merge_ordered(
+            total,
+            df,
+            left_on="timestamp",
+            right_on="timestamp",
+            how="outer",
+        )
+        total.sort_values(by="timestamp", inplace=True)
+        print(f"Merged: {len(df)} items. {i + 1}/{total_count}")
+        del df
+        gc.collect()
+        i += 1
+    del dfs
+    gc.collect()
+    return total
+
 if __name__ == "__main__":
 
-    rows = IBTestProviderStubs.universe_rows()
+    rows = IBTestProviderStubs.universe_rows()[:10]
     
     sector_map = {row.uname:row.sector for row in rows}
     sub_sector_map = {row.uname:row.sub_sector for row in rows}
     region_map = {row.uname:row.region for row in rows}
     
-    dfs = []
+    dfs = {}
     for row in rows:
         
         file = IBTestProviderStubs.adjusted_file(
@@ -31,118 +54,30 @@ if __name__ == "__main__":
         assert df.timestamp.is_monotonic_increasing
         
         # prices
-        dfs.append(
-            # Convert Pandas DataFrame to Polars DataFrame
-            
-            pl.DataFrame(
+        dfs[row.uname] = pd.DataFrame(
                 {
                     "timestamp": df.timestamp.dt.floor("D").values,
                     row.uname: df.adjusted.values
                 }
             )
-        )
     
+    
+    for row1 in rows:
+        for row2 in rows:
+            if row1.uname == row2.uname:
+                corr = 1
+            else:
+                df = merge_dataframes_pandas(
+                    [dfs[row1.uname], dfs[row2.uname]]
+                )
+                print(df)
+                exit()
+                
     # merge the dataframes
     print("Merging dataframes")
-    prices_corr = pd.DataFrame(columns=["timestamp"])
+    # prices_corr = pd.DataFrame(columns=["timestamp"])
     
-    def chunkify(l, n):
-        """Yield n number of striped chunks from l."""
-        new_list = []
-        for i in range(0, n):
-            chunk = l[i::n]
-            if chunk != []:
-                new_list.append(chunk)
-        return new_list
     
-    def merge_dataframes_pandas(dfs: list[pd.DataFrame]) -> pd.DataFrame:
-        total_count = len(dfs)
-        i = 0
-        total = pd.DataFrame(columns=["timestamp"])
-        while len(dfs) > 0:
-            df = dfs.pop(-1)
-            # prices_corr = prices_corr.set_index("timestamp").combine_first(df.set_index("timestamp")).reset_index()
-            total = pd.merge_ordered(
-                total,
-                df,
-                left_on="timestamp",
-                right_on="timestamp",
-                how="outer",
-            )
-            total.sort_values(by="timestamp", inplace=True)
-            print(f"Merged: {len(df)} items. {i + 1}/{total_count}")
-            del df
-            gc.collect()
-            i += 1
-        del dfs
-        gc.collect()
-        return total
-    
-    def merge_dataframes_polars(dfs: list[pd.DataFrame]) -> pd.DataFrame:
-        total_count = len(dfs)
-        i = 0
-        total = dfs.pop(-1)
-        while len(dfs) > 0:
-            df = dfs.pop(-1)
-            total = total.join(
-                df,
-                
-                on="timestamp",
-                how="outer",
-            )
-            
-            total = total.with_columns([
-                pl.when(total['timestamp'].is_null()) \
-                    .then(total['timestamp_right']) \
-                    .otherwise(total['timestamp']) \
-                    .alias("timestamp")
-            ])
-            total = total.drop('timestamp_right')
-            print(f"Merged: {len(df)} items. {i + 1}/{total_count}")
-            del df
-            gc.collect()
-            i += 1
-        del dfs
-        gc.collect()
-        return total
-    
-    # df = merge_dataframes_polars(dfs)
-    for n_jobs in (24, 12, 6, 3):
-        chunks = chunkify(dfs, n_jobs)
-        assert len(chunks) == n_jobs
-        dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
-            joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
-        )
-        assert len(dfs) == n_jobs
-        
-        # n_jobs=12
-        # chunks = chunkify(dfs, n_jobs)
-        # assert len(chunks) == n_jobs
-        # dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
-        #     joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
-        # )
-        # assert len(dfs) == n_jobs
-        
-        # n_jobs=6
-        # chunks = chunkify(dfs, n_jobs)
-        # assert len(chunks) == n_jobs
-        # dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
-        #     joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
-        # )
-        # assert len(chunks) == n_jobs
-        
-        # n_jobs=3
-        # chunks = chunkify(dfs, n_jobs)
-        # assert len(chunks) == n_jobs
-        # dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
-        #     joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
-        # )
-        # assert len(chunks) == n_jobs
-    
-    print("DONE")
-    # df = merge_dataframes_polars(dfs)
-    
-    exit()
     
     gc.collect()
     
@@ -222,3 +157,100 @@ def correlate(df: pd.DataFrame) -> pd.DataFrame:
             
             
             
+            # def chunkify(l, n):
+    #     """Yield n number of striped chunks from l."""
+    #     new_list = []
+    #     for i in range(0, n):
+    #         chunk = l[i::n]
+    #         if chunk != []:
+    #             new_list.append(chunk)
+    #     return new_list
+    
+    # def merge_dataframes_pandas(dfs: list[pd.DataFrame]) -> pd.DataFrame:
+    #     total_count = len(dfs)
+    #     i = 0
+    #     total = pd.DataFrame(columns=["timestamp"])
+    #     while len(dfs) > 0:
+    #         df = dfs.pop(-1)
+    #         # prices_corr = prices_corr.set_index("timestamp").combine_first(df.set_index("timestamp")).reset_index()
+    #         total = pd.merge_ordered(
+    #             total,
+    #             df,
+    #             left_on="timestamp",
+    #             right_on="timestamp",
+    #             how="outer",
+    #         )
+    #         total.sort_values(by="timestamp", inplace=True)
+    #         print(f"Merged: {len(df)} items. {i + 1}/{total_count}")
+    #         del df
+    #         gc.collect()
+    #         i += 1
+    #     del dfs
+    #     gc.collect()
+    #     return total
+    
+    # def merge_dataframes_polars(dfs: list[pd.DataFrame]) -> pd.DataFrame:
+    #     total_count = len(dfs)
+    #     i = 0
+    #     total = dfs.pop(-1)
+    #     while len(dfs) > 0:
+    #         df = dfs.pop(-1)
+    #         total = total.join(
+    #             df,
+                
+    #             on="timestamp",
+    #             how="outer",
+    #         )
+            
+    #         total = total.with_columns([
+    #             pl.when(total['timestamp'].is_null()) \
+    #                 .then(total['timestamp_right']) \
+    #                 .otherwise(total['timestamp']) \
+    #                 .alias("timestamp")
+    #         ])
+    #         total = total.drop('timestamp_right')
+    #         print(f"Merged: {len(df)} items. {i + 1}/{total_count}")
+    #         del df
+    #         gc.collect()
+    #         i += 1
+    #     del dfs
+    #     gc.collect()
+    #     return total
+    
+    # # df = merge_dataframes_polars(dfs)
+    # for n_jobs in (24, 12, 6, 3):
+    #     chunks = chunkify(dfs, n_jobs)
+    #     assert len(chunks) == n_jobs
+    #     dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
+    #         joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
+    #     )
+    #     assert len(dfs) == n_jobs
+        
+    #     # n_jobs=12
+    #     # chunks = chunkify(dfs, n_jobs)
+    #     # assert len(chunks) == n_jobs
+    #     # dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
+    #     #     joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
+    #     # )
+    #     # assert len(dfs) == n_jobs
+        
+    #     # n_jobs=6
+    #     # chunks = chunkify(dfs, n_jobs)
+    #     # assert len(chunks) == n_jobs
+    #     # dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
+    #     #     joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
+    #     # )
+    #     # assert len(chunks) == n_jobs
+        
+    #     # n_jobs=3
+    #     # chunks = chunkify(dfs, n_jobs)
+    #     # assert len(chunks) == n_jobs
+    #     # dfs = joblib.Parallel(n_jobs=n_jobs, backend="loky")(
+    #     #     joblib.delayed(merge_dataframes_polars)(chunk) for chunk in chunks
+    #     # )
+    #     # assert len(chunks) == n_jobs
+    
+    # print("DONE")
+    # df = merge_dataframes_polars(dfs)
+    
+    # exit()
