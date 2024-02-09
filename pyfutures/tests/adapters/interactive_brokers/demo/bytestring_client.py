2486ab866421a9121978ba176ca112cf3dca87f8
Freddie Hill <freddiehill000gmail.com>
2024-02-09 21:07:16 +0000

    bytestring_client

--- pyfutures/tests/adapters/interactive_brokers/demo/bytestring_client.py
+++ pyfutures/tests/adapters/interactive_brokers/demo/bytestring_client.py
@@ -12,9 +12,10 @@ import functools
 import types
 
 
-from ibapi.contract import Contract as Contract
+from ibapi.contract import Contract
 from nautilus_trader.common.component import init_logging
 from nautilus_trader.common.enums import LogLevel
+init_logging(level_stdout=LogLevel.DEBUG)
 
 
 
@@ -39,64 +40,96 @@ from nautilus_trader.common.enums import LogLevel
 #         # and then invoke __new__ in type metaclass
 #         return super().__new__(mcs, name, bases, attrs)
 #
-class DecorateMethods(type):
-    """ Decorate all methods of the class with the decorator provided """
 
+
+from functools import wraps
+def wrapper(method):
+    # @wraps(method)
+    print("wrapper applied")
+    def wrapped(*args, **kwargs):
+        print("log to file")
+        method(*args, **kwargs)
+    return wrapped
+
+
+class DecorateMethods(type):
+    """ Decorate all methods of the superclass with the decorator provided """
     def __new__(cls, name, bases, attrs, **kwargs):
         try:
             decorator = kwargs['decorator']
         except KeyError:
-            raise ValueError('Please provide the "decorator" argument, eg. '
-                             'MyClass(..., metaclass=DecorateMethods, decorator=my_decorator)')
+            raise ValueError('Please provide the "decorator" argument')
 
         exclude = kwargs.get('exclude', [])
 
+        # Iterate through attrs to access instance methods
         for attr_name, attr_value in attrs.items():
-
+            print(attr_name)
             if isinstance(attr_value, types.FunctionType) and \
-                    attr_name not in exclude and \
-                    not attr_name.startswith('__'):
+                attr_name not in exclude and \
+                not attr_name.startswith('__'):
                 attrs[attr_name] = decorator(attr_value)
 
         return super(DecorateMethods, cls).__new__(cls, name, bases, attrs)
 
 
-def log_to_file(one, two, three, four): # *args, **kwargs):
-    # print(args, kwargs)
-    print(one, two, three, four)
+def wrap_methods(client):
+    print(client.__dict__)
+    for k, v in client.__dict__.items():
+        if isinstance(k, types.FunctionType) and \
+                not k.startswith("__"):
+            client[k] = wrapper(client[k])
+    return client
 
-class LoggingInteractiveBrokersClient(InteractiveBrokersClient,metaclass=DecorateMethods, decorator=log_to_file):
-    def __init__(
-        self,
-        loop: asyncio.AbstractEventLoop,
-        msgbus: MessageBus,
-        cache: Cache,
-        clock: LiveClock,
-        logger: Logger,
-        host: str = "127.0.0.1",
-        port: int = 7497,
-        client_id: int = 1,
-        api_log_level: int = logging.ERROR,
-        request_timeout_seconds: int | None = None,
-    ):
-        super().__init__(
-            loop=loop,
-            msgbus=msgbus,
-            cache=cache,
-            clock=clock,
-            logger=logger,
-            host=host,
-            port=port,
-            client_id=client_id,
-            api_log_level=api_log_level,
-            request_timeout_seconds=request_timeout_seconds,
-        )
+# def log_to_file(func):
+#     @functools.wraps(func)  # Preserve metadata
+#     def wrapper(*args, **kwargs):
+#         print("log to file")
+#         print(args, kwargs)  # Access actual arguments
+#         result = func(*args, **kwargs)
+#         return result
+#     return wrapper
+
+# class LoggingInteractiveBrokersClient(InteractiveBrokersClient,metaclass=DecorateMethods,decorator=wrapper):
+#     def __init__(
+#         self,
+#         loop: asyncio.AbstractEventLoop,
+#         msgbus: MessageBus,
+#         cache: Cache,
+#         clock: LiveClock,
+#         host: str = "127.0.0.1",
+#         port: int = 7497,
+#         client_id: int = 1,
+#         api_log_level: int = logging.ERROR,
+#         request_timeout_seconds: int | None = None,
+#     ):
+#         super().__init__(
+#             loop=loop,
+#             msgbus=msgbus,
+#             cache=cache,
+#             clock=clock,
+#             host=host,
+#             port=port,
+#             client_id=client_id,
+#             api_log_level=api_log_level,
+#             request_timeout_seconds=request_timeout_seconds,
+#         )
 
 
-logger = Logger(name="pytest")
 
 
-client = LoggingInteractiveBrokersClient(
+async def main():
+    contract = Contract()
+    contract.secType="FUT"
+    contract.exchange="SNFE"
+    contract.symbol="XT"
+    contract.tradingClass="XT"
+    contract.currency="AUD"
+
+    await client.connect()
+    await client.request_contract_details(contract=contract)
+
+client = InteractiveBrokersClient(
     loop=asyncio.get_event_loop(),
     msgbus=MessageBus(
         TestIdStubs.trader_id(),
@@ -104,14 +137,11 @@ client = LoggingInteractiveBrokersClient(
     ),
     cache=TestComponentStubs.cache(),
     clock=LiveClock(),
-    logger=logger,
     port=4002,
 )
-contract = IBContract()
-contract.secType="FUT"
-contract.exchange="SNFE"
-contract.symbol="XT"
-contract.tradingClass="XT"
-contract.currency="AUD"
 
-asyncio.run(client.request_contract_details(contract=contract))
+
+client = wrap_methods(client)
+
+
+asyncio.get_event_loop().run_until_complete(main())
