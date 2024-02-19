cca2e316c4c6702daf3d77b29ba2878f7597a35b
Freddie Hill <freddiehill000gmail.com>
2024-02-19 10:30:00 +0000

    stats

---
+++ pyfutures/tests/stats/test_stats.py
@@ -0,0 +1,12 @@
+import pytest
+from pyfutures.stats.stats import Stats
+import asyncio
+
+
+def test_stats(client):
+    stats = Stats(client=client)
+    # stats._get_contract_price(exchange="CME", symbol="DC", url="https://contract.ibkr.info/v3.10/index.php?action=Details&site=GEN&conid=668447027")
+    # stats.calc()
+    stats.test_last_close()
+
+
