8bc138ea3740b1783db6035e5d5f6f54198b9c3b
Freddie Hill <freddiehill000@gmail.com>
2024-03-07 02:43:14 +0000

    logging trading_node tests

--- pyfutures/tests/demo/adapter/test_logging.py
+++ pyfutures/tests/demo/adapter/test_logging.py
@@ -1,120 +1,75 @@
-import asyncio
+# Tests if:
+# - python module logging (used by Pyfutures IB Client and Historic)
+# - nautilus Logger
+# can viewed in the same stdout
+#
+#
 
-from nautilus_trader.adapters.interactive_brokers.common import IB_VENUE
-from nautilus_trader.common import Environment
+
+
+import logging
+import sys
+from nautilus_trader.common.component import Logger
 from nautilus_trader.common.component import init_logging
 from nautilus_trader.common.enums import LogLevel
-from nautilus_trader.config import LiveExecEngineConfig
-from nautilus_trader.config import TradingNodeConfig
-from nautilus_trader.live.config import RoutingConfig
-from nautilus_trader.live.node import TradingNode
-from nautilus_trader.model.data import BarType
-from nautilus_trader.model.enums import OrderSide
-from nautilus_trader.model.objects import Price
-from nautilus_trader.config import LoggingConfig
 
-#
-from pytower.tests.stubs.strategies import BuyOnBarX
-
-from pyfutures.adapter.config import (
-    InteractiveBrokersDataClientConfig,
-)
-from pyfutures.adapter.config import (
-    InteractiveBrokersExecClientConfig,
-)
-from pyfutures.adapter.config import (
-    InteractiveBrokersInstrumentProviderConfig,
-)
 from pyfutures.adapter.factories import (
     InteractiveBrokersLiveDataClientFactory,
 )
 from pyfutures.adapter.factories import (
     InteractiveBrokersLiveExecClientFactory,
 )
+from pyfutures.adapter.config import (
+    InteractiveBrokersDataClientConfig,
+)
+from pyfutures.adapter.config import (
+    InteractiveBrokersExecClientConfig,
+)
 
 
-# init_logging(level_stdout=LogLevel.DEBUG)
+from nautilus_trader.cache.cache import Cache
+from nautilus_trader.common.component import LiveClock
+from nautilus_trader.common.component import MessageBus
+from nautilus_trader.model.identifiers import TraderId
+import asyncio
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-def test_strategy_logging():
-    # Arrange
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    # monkeypatch.setenv("BINANCE_API_KEY", "SOME_API_KEY")
-    # monkeypatch.setenv("BINANCE_API_SECRET", "SOME_API_SECRET")
-    # row = IBTestProviderStubs.universe_rows()[0]
-    bar_type = BarType.from_str("EUR.GBP=CASH.IDEALPRO-5-SECOND-BID-EXTERNAL")
-
-    provider_config = InteractiveBrokersInstrumentProviderConfig(
-        chain_filters={
-            "FMEU": lambda x: x.contract.localSymbol[-1] not in ("M", "D"),
-        },
-        parsing_overrides={
-            "MIX": {
-                "price_precision": 0,
-                "price_increment": Price(5, 0),
-            },
-        },
-        load_ids=[bar_type.instrument_id.value],
-    )
-
-    config = TradingNodeConfig(
-        # logging=LoggingConfig(bypass_logging=True),
-        environment=Environment.LIVE,
-        logging=LoggingConfig(
-            log_level="DEBUG",
-            log_directory="logdir",
-            log_level_file="DEBUG",
-            log_file_format="log",
-            # log_component_levels={"Portfolio": "INFO"},
+def test_dual_logging_components():
+    clock=LiveClock()
+    data_client =  InteractiveBrokersLiveDataClientFactory.create(
+        loop=asyncio.get_event_loop(),
+        name="str",
+        config=InteractiveBrokersDataClientConfig(),
+        msgbus = MessageBus(
+            trader_id=TraderId("TESTER-000"),
+            clock=clock,
         ),
-        data_clients={
-            "INTERACTIVE_BROKERS": InteractiveBrokersDataClientConfig(
-                instrument_provider=provider_config, routing=RoutingConfig(default=True)
-            ),
-        },
-        exec_clients={
-            "INTERACTIVE_BROKERS": InteractiveBrokersExecClientConfig(
-                instrument_provider=provider_config, routing=RoutingConfig(default=True)
-            ),
-        },
-        timeout_disconnection=1.0,  # Short timeout for testing
-        timeout_post_stop=1.0,  # Short timeout for testing
-        exec_engine=LiveExecEngineConfig(
-            reconciliation=False,
-            inflight_check_interval_ms=0,
-            debug=True,
-        ),
+        cache=Cache(),
+        clock=clock,
     )
-    node = TradingNode(config=config, loop=loop)
+    python_logger = logging.getLogger("test_logging")
+    python_logger.setLevel(logging.DEBUG)
+    python_logger.info("Here is python logger")
 
-    # add instrument to the cache,
-    node.add_data_client_factory(
-        "INTERACTIVE_BROKERS", InteractiveBrokersLiveDataClientFactory
-    )
-    node.add_exec_client_factory(
-        "INTERACTIVE_BROKERS", InteractiveBrokersLiveExecClientFactory
-    )
 
-    node.build()
-    strategy = BuyOnBarX(
-        index=1,
-        bar_type=bar_type,
-        order_side=OrderSide.BUY,
-        quantity=1,
-    )
-    strategytwo = BuyOnBarX(
-        index=1,
-        bar_type=bar_type,
-        order_side=OrderSide.BUY,
-        quantity=1,
-    )
+    init_logging(level_stdout=LogLevel.DEBUG)
+    nautilus_logger = Logger("test_logging")
+    nautilus_logger.info("Here is nautilus logger")
 
-    # exec_client_id = ClientId("IB")
-    node.trader.add_strategy(strategy)
-    node.trader.add_strategy(strategytwo)
+def test_dual_logging():
+    """
+        should be able to call this with python or pytest
+    """
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    python_logger = logging.getLogger("test_logging")
+    python_logger.setLevel(logging.DEBUG)
+    python_logger.info("Here is python logger")
 
-    node.portfolio.set_specific_venue(IB_VENUE)
 
-    node.run()
+    init_logging(level_stdout=LogLevel.DEBUG)
+    nautilus_logger = Logger("test_logging")
+    nautilus_logger.info("Here is nautilus logger")
+
+
+test_dual_logging_components()
---
+++ pyfutures/tests/demo/adapter/test_trading_node.py
@@ -0,0 +1,131 @@
+import asyncio
+
+from nautilus_trader.adapters.interactive_brokers.common import IB_VENUE
+from nautilus_trader.common import Environment
+from nautilus_trader.common.component import init_logging
+from nautilus_trader.common.enums import LogLevel
+from nautilus_trader.config import LiveExecEngineConfig
+from nautilus_trader.config import TradingNodeConfig
+from nautilus_trader.live.config import RoutingConfig
+from nautilus_trader.live.node import TradingNode
+from nautilus_trader.model.data import BarType
+from nautilus_trader.model.enums import OrderSide
+from nautilus_trader.model.objects import Price
+from nautilus_trader.config import LoggingConfig
+
+#
+from pytower.tests.stubs.strategies import BuyOnBarX
+
+from pyfutures.adapter.config import (
+    InteractiveBrokersDataClientConfig,
+)
+from pyfutures.adapter.config import (
+    InteractiveBrokersExecClientConfig,
+)
+from pyfutures.adapter.config import (
+    InteractiveBrokersInstrumentProviderConfig,
+)
+from pyfutures.adapter.factories import (
+    InteractiveBrokersLiveDataClientFactory,
+)
+from pyfutures.adapter.factories import (
+    InteractiveBrokersLiveExecClientFactory,
+)
+
+
+# init_logging(level_stdout=LogLevel.DEBUG)
+
+import sys
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+init_logging(level_stdout=LogLevel.DEBUG)
+
+
+def test_trading_node():
+    # Arrange
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # monkeypatch.setenv("BINANCE_API_KEY", "SOME_API_KEY")
+    # monkeypatch.setenv("BINANCE_API_SECRET", "SOME_API_SECRET")
+    # row = IBTestProviderStubs.universe_rows()[0]
+    bar_type = BarType.from_str("EUR.GBP=CASH.IDEALPRO-5-SECOND-BID-EXTERNAL")
+
+    provider_config = InteractiveBrokersInstrumentProviderConfig(
+        chain_filters={
+            "FMEU": lambda x: x.contract.localSymbol[-1] not in ("M", "D"),
+        },
+        parsing_overrides={
+            "MIX": {
+                "price_precision": 0,
+                "price_increment": Price(5, 0),
+            },
+        },
+        load_ids=[bar_type.instrument_id.value],
+    )
+
+    config = TradingNodeConfig(
+        # logging=LoggingConfig(bypass_logging=True),
+        environment=Environment.LIVE,
+        # logging=LoggingConfig(
+            # log_level="debug",
+            # log_directory="logdir",
+            # log_level_file="DEBUG",
+            # log_file_format="log",
+            # log_component_levels={"Portfolio": "INFO"},
+        # ),
+        data_clients={
+            "INTERACTIVE_BROKERS": InteractiveBrokersDataClientConfig(
+                instrument_provider=provider_config, routing=RoutingConfig(default=True)
+            ),
+        },
+        exec_clients={
+            "INTERACTIVE_BROKERS": InteractiveBrokersExecClientConfig(
+                instrument_provider=provider_config, routing=RoutingConfig(default=True)
+            ),
+        },
+        timeout_disconnection=1.0,  # Short timeout for testing
+        timeout_post_stop=1.0,  # Short timeout for testing
+        exec_engine=LiveExecEngineConfig(
+            reconciliation=False,
+            inflight_check_interval_ms=0,
+            debug=True,
+        ),
+    )
+    node = TradingNode(config=config, loop=loop)
+
+    # add instrument to the cache,
+    node.add_data_client_factory(
+        "INTERACTIVE_BROKERS", InteractiveBrokersLiveDataClientFactory
+    )
+    node.add_exec_client_factory(
+        "INTERACTIVE_BROKERS", InteractiveBrokersLiveExecClientFactory
+    )
+
+
+    node.build()
+    strategy = BuyOnBarX(
+        index=1,
+        bar_type=bar_type,
+        order_side=OrderSide.BUY,
+        quantity=1,
+    )
+    strategytwo = BuyOnBarX(
+        index=1,
+        bar_type=bar_type,
+        order_side=OrderSide.BUY,
+        quantity=1,
+    )
+
+    # exec_client_id = ClientId("IB")
+    node.trader.add_strategy(strategy)
+    node.trader.add_strategy(strategytwo)
+
+    node.portfolio.set_specific_venue(IB_VENUE)
+
+    node.run()
+
+
+test_trading_node()
