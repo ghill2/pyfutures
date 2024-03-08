d36a3350cfa3e4f330d88c1e5032737c09a8e52d
George Hill <georgehill010@gmail.com>
2024-03-08 13:08:30 +0000

    spread_values

---
+++ pyfutures/tests/import_spread_values.py
@@ -0,0 +1,26 @@
+from pyfutures.tests.test_kit import IBTestProviderStubs
+
+"""
+once per hour should be enough
+"""
+if __name__ == "__main__":
+    
+    rows = IBTestProviderStubs.universe_rows(
+        # filter=["ECO"],
+    )
+    
+    
+    for row in rows:
+        pass
+    
+        # load bars as dataframe
+    
+        # filter bars in liquid hours
+        
+        # return dataframe for each hour
+        
+        # return a random row in the dataframe for each dataframe
+        
+        # average all random rows
+        
+        
