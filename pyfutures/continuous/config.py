ad2f2a27ffdc77c5b41ccbc5e9b449612364c60d
George Hill <georgehill010@gmail.com>
2024-03-13 16:48:34 +0000

    auto commit 2024-03-13 16:48:33

--- pyfutures/continuous/config.py
+++ pyfutures/continuous/config.py
@@ -8,7 +8,6 @@ from nautilus_trader.model.identifiers import InstrumentId
 
 from pyfutures.continuous.contract_month import ContractMonth
 from pyfutures.continuous.cycle import RollCycle
-from pyfutures.schedule.schedule import MarketSchedule
 
 
 # An integer constrained to values <= 0
@@ -17,12 +16,18 @@ NonPositiveInt = Annotated[int, Meta(le=0)]
 
 class RollConfig(NautilusConfig, frozen=True):
     """
-    hold_cycle: The contract cycle string we want to hold
-    priced_cycle: The contract cycle string of available prices
-    roll_offset: The day, relative to the expiry date, when we usually roll
-    carry_offset: The number of contracts forward or backwards defines carry in the priced roll cycle
-    approximate_expiry_offset: The offset, relative to the first of the contract month that the expiry date approximately occurs
+    Configuration for the rolling of contracts
+
+    Parameters
+    ----------
+    hold_cycle : RollCycle, The contract cycle string we want to hold
+    priced_cycle : RollCycle, The contract cycle string of available prices
+    roll_offset : NonPositiveInt, The day, relative to the expiry date, when we usually roll
+    carry_offset : int, The number of contracts forward or backwards defines carry in the priced roll cycle
+    approximate_expiry_offset : The offset, relative to the first of the contract month that the expiry date approximately occurs
+        The cache configuration.
     """
+
     instrument_id: InstrumentId
     hold_cycle: RollCycle
     priced_cycle: RollCycle
@@ -30,6 +35,7 @@ class RollConfig(NautilusConfig, frozen=True):
     approximate_expiry_offset: NonNegativeInt
     carry_offset: Literal[1, -1]
     skip_months: list[ContractMonth] | None = None
+
 
 class ContractChainConfig(NautilusConfig, frozen=True):
     bar_type: BarType
